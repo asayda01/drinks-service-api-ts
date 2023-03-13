e974e4897320b1315937e160892c183733539df1
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-13 13:25:45 +0000

    feat: added a new GET request for coffee endpoint
     feat: Implement new GET request for coffeelover endpoint and refactored its test file .
    
    - deleted coffee_lover.ts file
    - deleted coffee_lover.test.ts file

---
+++ src/controllers/coffee_request.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { app } from "../app";
+
+describe("Test home API endpoint request", () => {
+
+  test("GET should return correct message", async () => {
+
+    const res = await request(app).get("/coffeelover");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("I like coffee!");
+
+  });
+
+  test("GET /coffee without a parameter should return correct object", async () => {
+
+    const res = await request(app).get("/coffee");
+    expect(res.statusCode).toEqual(200);
+    
+    expect(res.body).toEqual({
+
+      drinkType: "Coffee",
+      name: "Latte",
+
+    });
+
+  });
+
+});
