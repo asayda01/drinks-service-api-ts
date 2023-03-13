e50a896f4e31144ce59d4c7bb9e7779494c51430
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-13 16:08:45 +0000

    added more test for the coffee controller
    added more test for the coffee controller

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -15,7 +15,7 @@ describe("Test home API endpoint request", () => {
 
     const res = await request(app).get("/coffee");
     expect(res.statusCode).toEqual(200);
-    
+
     expect(res.body).toEqual({
 
       drinkType: "Coffee",
@@ -24,5 +24,33 @@ describe("Test home API endpoint request", () => {
     });
 
   });
+
+  test('GET /coffee with different param should return correct object', async () => {
+    
+    const res = await request(app).get('/coffee').query({ coffeeName: 'Macchiato' });
+    
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+
+      drinkType: 'Coffee',
+      name: 'Macchiato',
+
+    });
+
+});
+
+test('GET /coffee with different param should return correct object', async () => {
+    
+  const res = await request(app).get('/coffee').query({ coffeeName: 'Frappuccino' });
+  
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+
+    drinkType: 'Coffee',
+    name: 'Frappuccino',
+
+  });
+
+});
 
 });
